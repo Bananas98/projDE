37182fdea429bbdcee32878c57360831edfb571b
David Nasirov <[davidnasirov@gmail.com]>
2020-11-30 13:52:31 +0200

    44 refactor hibernate dao

--- src/main/java/departmentmanagement/dao/hibernate/HibernateDepartmentImpl.java
+++ src/main/java/departmentmanagement/dao/hibernate/HibernateDepartmentImpl.java
@@ -5,6 +5,7 @@ import departmentmanagement.dao.HibernateUtil;
 import departmentmanagement.dao.interfaces.Dao;
 import departmentmanagement.model.Department;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 
 import java.util.List;
@@ -13,106 +14,49 @@ public class HibernateDepartmentImpl implements Dao<Department> {
 
     @Override
     public Department getById(Integer id) {
-        Session session = null;
-        Department department = null;
-        try {
-            session = HibernateUtil.getSessionFactory().openSession();
-            session.beginTransaction();
-            department = session.get(Department.class, id);
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (session != null && session.isOpen()) {
-                session.close();
-            }
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        try (Session session = sessionFactory.openSession()) {
+            return session.get(Department.class, id);
         }
-        return department;
     }
 
     public Department findByName(String name) {
-        Session session = null;
-        Department department = null;
-        try {
-            session = HibernateUtil.getSessionFactory().openSession();
-            session.beginTransaction();
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        try (Session session = sessionFactory.openSession()) {
             Query<Department> query = session.
                     createQuery("from departmentmanagement.model.Department where name=:name");
             query.setParameter("name", name);
-            department = query.uniqueResult();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (session != null && session.isOpen()) {
-                session.close();
-            }
+            return query.uniqueResult();
         }
-        return department;
     }
 
     @Override
     public void delete(Integer id) {
-        Session session = null;
-        try {
-            session = HibernateUtil.getSessionFactory().openSession();
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        try (Session session = sessionFactory.openSession()){
             session.beginTransaction();
-            Query query = session.createQuery("DELETE FROM departmentmanagement.model.Department WHERE id = :id");
-            query.setParameter("id", id);
-            query.executeUpdate();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (session != null && session.isOpen()) {
-                session.close();
+            Department department = session.get(Department.class,id);
+            if (department != null){
+                session.delete(department);
             }
-        }
-    }
-
-    private void create(Department department) {
-        Session session = null;
-        try {
-            session = HibernateUtil.getSessionFactory().openSession();
-            session.beginTransaction();
-            session.save(department);
             session.getTransaction().commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (session != null && session.isOpen()) {
-                session.close();
-            }
-        }
-    }
-
-    private void update(Department department) {
-        Session session = null;
-        try {
-            session = HibernateUtil.getSessionFactory().openSession();
-            session.beginTransaction();
-            session.update(department);
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (session != null && session.isOpen()) {
-                session.close();
-            }
         }
     }
 
     public List<Department> getAll() {
-        return HibernateUtil.getSessionFactory().openSession()
-                .createQuery("from departmentmanagement.model.Department", Department.class).list();
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        try(Session session = sessionFactory.openSession()){
+            return session.createQuery("from departmentmanagement.model.Department", Department.class).list();
+        }
     }
 
     @Override
     public void createOrUpdate(Department department) {
-        if (department.getId() == null) {
-            create(department);
-        } else {
-            update(department);
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        try (Session session = sessionFactory.openSession()){
+            session.beginTransaction();
+            session.saveOrUpdate(department);
+            session.getTransaction().commit();
         }
     }
 }
--- src/main/java/departmentmanagement/dao/hibernate/HibernateEmployeeImpl.java
+++ src/main/java/departmentmanagement/dao/hibernate/HibernateEmployeeImpl.java
@@ -2,11 +2,9 @@ package departmentmanagement.dao.hibernate;
 
 import departmentmanagement.dao.HibernateUtil;
 import departmentmanagement.dao.interfaces.Dao;
-import departmentmanagement.model.Department;
 import departmentmanagement.model.Employee;
-import org.hibernate.Criteria;
 import org.hibernate.Session;
-import org.hibernate.criterion.Restrictions;
+import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 
 import java.util.List;
@@ -16,109 +14,53 @@ public class HibernateEmployeeImpl implements Dao<Employee> {
 
     @Override
     public Employee getById(Integer id) {
-        Session session = null;
-        Employee employee = null;
-        try {
-            session = HibernateUtil.getSessionFactory().openSession();
-            session.beginTransaction();
-            employee = session.get(Employee.class, id);
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (session != null && session.isOpen()) {
-                session.close();
-            }
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        try (Session session = sessionFactory.openSession()) {
+            return session.get(Employee.class, id);
         }
-        return employee;
     }
 
     @Override
     public void delete(Integer id) {
-        Session session = null;
-        try {
-            session = HibernateUtil.getSessionFactory().openSession();
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        try (Session session = sessionFactory.openSession()){
             session.beginTransaction();
-            Query query = session.createQuery("DELETE FROM departmentmanagement.model.Employee WHERE id = :id");
-            query.setParameter("id", id);
-            query.executeUpdate();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (session != null && session.isOpen()) {
-                session.close();
+            Employee employee = session.get(Employee.class,id);
+            if (employee != null){
+                session.delete(employee);
             }
+            session.getTransaction().commit();
         }
     }
+
 
     @Override
     public void createOrUpdate(Employee employee) {
-        if (employee.getId() == null) {
-            create(employee);
-        } else {
-            update(employee);
-        }
-    }
-
-    private void create(Employee employee) {
-        Session session = null;
-        try {
-            session = HibernateUtil.getSessionFactory().openSession();
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        try (Session session = sessionFactory.openSession()) {
             session.beginTransaction();
-            session.save(employee);
+            session.saveOrUpdate(employee);
             session.getTransaction().commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (session != null && session.isOpen()) {
-                session.close();
-            }
-        }
-    }
-
-    private void update(Employee employee) {
-        Session session = null;
-        try {
-            session = HibernateUtil.getSessionFactory().openSession();
-            session.beginTransaction();
-            session.update(employee);
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (session != null && session.isOpen()) {
-                session.close();
-            }
         }
     }
 
     public List<Employee> getAll(Integer departmentId) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        Query<Employee> query  = session
-                .createQuery("from departmentmanagement.model.Employee where id_department=:departmentId", Employee.class);
-        query.setParameter("departmentId",departmentId);
-        return query.list();
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        try(Session session = sessionFactory.openSession()) {
+            Query<Employee> query = session
+                    .createQuery("from Employee where id_department=:departmentId", Employee.class);
+            query.setParameter("departmentId", departmentId);
+            return query.list();
+        }
     }
 
     public Employee findByEmail(String email) {
-        Session session = null;
-        Employee employee = null;
-        try {
-            session = HibernateUtil.getSessionFactory().openSession();
-            session.beginTransaction();
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        try (Session session = sessionFactory.openSession()) {
             Query<Employee> query = session.
-                    createQuery("from departmentmanagement.model.Employee where mail=:email");
+                    createQuery("from Employee where mail=:email");
             query.setParameter("email", email);
-            employee = query.uniqueResult();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (session != null && session.isOpen()) {
-                session.close();
-            }
+            return query.uniqueResult();
         }
-        return employee;
     }
 }
